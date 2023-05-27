f5942b1f7f2b2c20e65a045611fd791ddf8dccfe
Chester Luke Maligaso <chestermaligaso29@gmail.com>
2023-05-27 20:35:06 +0800

    delete pics

--- admin_header.php
+++ admin_header.php
@@ -23,7 +23,6 @@ if(isset($message)){
          <a href="admin_orders.php">Orders</a>
          <a href="admin_users.php">Users</a>
          <a href="admin_contacts.php">Messages</a>
-         <a href="admin_announcement.php">Announcement</a>
       </nav>
 
       <div class="icons">
